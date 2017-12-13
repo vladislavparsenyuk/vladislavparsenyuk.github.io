c82f17d77c7e01341317253903872a3b2cdd7e99
Vladislav Parsenyuk <vladislavparsenyuk@gmail.com>
2017-12-13 17:02:50 +0200

    Text improvement

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 $(function () {
     'use strict';
 
-    var $cv = $('#cv');
-    var $tree = $('#tree');
-    var $about = $cv.find('.about');
-    var $name = $cv.find('.header-name');
-    var $es6 = $cv.find('.skills > span:eq(1)');
-    var $experience = $cv.find('.experience');
-    var $screenshot = $cv.find('.record:first .screenshot:last');
-    var $lastRecord = $cv.find('.record:last');
-    var $email = $cv.find('.contacts > ul > li:first');
-    var $feedback = $cv.find('.feedback:eq(0)');
-    var $photoshop = $cv.find('.skills > span:last');
-    var $entanglement = $cv.find('.main .entanglement');
+    var $cv           = $('#cv'),
+        $tree         = $('#tree'),
+        $about        = $cv.find('.about'),
+        $name         = $cv.find('.header-name'),
+        $es6          = $cv.find('.skills > span:eq(1)'),
+        $demos        = $cv.find('.record:eq(0)'),
+        $yooDo        = $cv.find('.record:eq(1)'),
+        $makeMeSmart  = $cv.find('.record:eq(2)'),
+        $screenshot   = $cv.find('.record:first .screenshot:last'),
+        $email        = $cv.find('.contacts > ul > li:first'),
+        $feedback     = $cv.find('.feedback:first'),
+        $photoshop    = $cv.find('.skills > span:last'),
+        $entanglement = $cv.find('.entanglement');
 
     new Butterfly({
         container: $cv,
@@ -20,14 +21,14 @@ $(function () {
             { el: $about, x: -44, y: 2, id: 'topPoint' },
             { el: $name, x: 300, y: 63 },
             { el: $es6, x: 31, y: 10 },
-            { el: $experience, x: 1, y: 16 },
+            { el: $demos, x: 1, y: 16 },
             { el: $screenshot, x: 200, y: 0 },
-            { el: $lastRecord, x: 1, y: 16 },
+            { el: $makeMeSmart, x: 1, y: 16 },
             { el: $tree, x: 33, y: 36, id: 'bottomPoint' },
             { el: $email, x: 9, y: 5 },
             { el: $feedback, x: 272, y: 36 },
-            { el: $experience, x: 1, y: 16 },
-            { el: $photoshop, x: 95, y: 12 },
+            { el: $yooDo, x: 1, y: 16 },
+            { el: $photoshop, x: 22, y: 12 },
         ],
     });
 
